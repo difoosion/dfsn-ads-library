55be92fcda924a6943a18bc2e71c184382e2e85f
Eugenio Estrada <eugenio.estrada@gmail.com>
2024-04-20 13:40:56 +0200

    Actualizar core.js

--- gallery/core.js
+++ gallery/core.js
@@ -2,10 +2,7 @@ import PhotoSwipe from 'https://cdnjs.cloudflare.com/ajax/libs/photoswipe/5.4.2/
 
 window.parent.window.dfsnGalleryOpen = function(element) {
     const options = {
-        dataSource: galleryItems[element.dataset.galleryName],
-        bgOpacity: 0.5,
-        padding: { top: 20, bottom: 40, left: 100, right: 100 },
-        showHideAnimationType: 'none'
+        dataSource: galleryItems[element.dataset.galleryName]
     };
     options.index = parseInt(element.dataset.index); // defines start slide index
     const pswp = new PhotoSwipe(options);
