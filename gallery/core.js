db19eca4229076de7e09e5617029107996efb4be
Eugenio Estrada <eugenio.estrada@gmail.com>
2024-04-20 13:29:42 +0200

    Refactor dfsnGalleryOpen function in gallery.js to use event.target instead of element

--- gallery/core.js
+++ gallery/core.js
@@ -1,7 +1,6 @@
 import PhotoSwipe from 'https://cdnjs.cloudflare.com/ajax/libs/photoswipe/5.4.2/photoswipe.esm.js';
 
-window.parent.window.dfsnGalleryOpen = function(event) {
-    const element = event.target;
+window.parent.window.dfsnGalleryOpen = function(element) {
     const options = {
         dataSource: galleryItems[element.dataset.galleryName],
         bgOpacity: 0.5,
