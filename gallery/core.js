dc1904c61d14dacf93198e242b8fd67fd23f4367
Eugenio Estrada <eugenio.estrada@gmail.com>
2024-04-20 18:26:06 +0200

    Update gallery.js to remove unused PhotoSwipeDynamicCaption plugin

--- gallery/core.js
+++ gallery/core.js
@@ -1,5 +1,4 @@
 import PhotoSwipe from 'https://cdnjs.cloudflare.com/ajax/libs/photoswipe/5.4.2/photoswipe.esm.js';
-import PhotoSwipeDynamicCaption from 'https://unpkg.com/photoswipe-dynamic-caption-plugin/photoswipe-dynamic-caption-plugin.esm.js';
 
 window.parent.window.dfsnGalleryOpen = function(element) {
     const lightbox = new PhotoSwipe({
@@ -11,13 +10,6 @@ window.parent.window.dfsnGalleryOpen = function(element) {
         console.log('itemData', itemData, index);
         return itemData;
     });*/
-
-    const captionPlugin = new PhotoSwipeDynamicCaption(lightbox, {
-        type: 'auto',
-        captionContent: (slide) => {
-            return slide.data.element.querySelector('img').getAttribute('alt');
-        }
-    });
     
     lightbox.init();
 };
